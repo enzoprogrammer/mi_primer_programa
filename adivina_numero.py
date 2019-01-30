3585547f81de8365257d12501df31e2df3e158ec
Enzo <enzogamerxbox@outlook.com>
2019-01-29 22:26:51 -0300

    Añadiendo archivo hello

---
+++ adivina_numero.py
@@ -0,0 +1,41 @@
+from random import randint, uniform,random
+
+numero_a_adivinar = randint(1,15)
+
+print("Bienvenido al juego de adivinar el numero del 1 al 15")
+print("")
+print("")
+print("Tendras 5 intentos para adivnar el numero")
+print("")
+print("")
+
+
+numero_usuario = int(input("Digite un numero: "))
+
+if numero_a_adivinar == numero_usuario:
+    print("Acertaste!")
+else:
+    print("Numero incorrecto")
+
+    numero_usuario = int(input("Digite un numero: "))
+    if numero_a_adivinar == numero_usuario:
+        print("Acertaste!")
+    else:
+        print("Numero incorrecto")
+        numero_usuario = int(input("Digite un numero: "))
+
+        if numero_a_adivinar == numero_usuario:
+            print("Acertaste!")
+        else:
+            print("Numero incorrecto")
+            numero_usuario = int(input("Digite un numero: "))
+            if numero_a_adivinar == numero_usuario:
+                print("Acertaste!")
+            else:
+                print("Numero incorrecto")
+                numero_usuario = int(input("Digite un numero: "))
+                if numero_a_adivinar == numero_usuario:
+                    print("Acertaste!")
+                else:
+                    print("Numero incorrecto, no te quedan mas intentos")
+                    print("El numero era: {}".format (numero_a_adivinar))
