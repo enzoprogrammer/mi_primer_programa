8ac40d592367dd32d33eb6884f853512d7fa5c24
Enzo <enzogamerxbox@outlook.com>
2019-02-01 00:15:55 -0300

    Calculadora

---
+++ programa_calculadora.py
@@ -0,0 +1,27 @@
+print("CALCULADORA... ")
+print(" ")
+operacion_elegida= input("Que operacion quiere hacer? (Suma / Resta / Multiplicacion / Division) :").upper()
+print(" ")
+primer_numero= int(input("Digite el primer numero: "))
+print(" ")
+segundo_numero= int(input("Digite el segundo numero: "))
+print(" ")
+
+if operacion_elegida== "SUMA":
+    resultado= primer_numero + segundo_numero
+    print("{} + {} = {}" .format(primer_numero, segundo_numero, resultado))
+elif operacion_elegida == "RESTA":
+    resultado= primer_numero - segundo_numero
+    print("{} - {} = {}".format(primer_numero, segundo_numero, resultado))
+elif operacion_elegida== "MULTIPLICACION":
+    resultado= primer_numero * segundo_numero
+    print("{} x {} = {}".format(primer_numero, segundo_numero, resultado))
+elif operacion_elegida== "DIVISION":
+    resultado= primer_numero / segundo_numero
+    print("{} / {} = {}".format(primer_numero, segundo_numero, resultado))
+else:
+    print("Elija bien las opciones...")
+
+
+
+
