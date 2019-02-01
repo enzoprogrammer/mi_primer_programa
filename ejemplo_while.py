2f64004019b0c75b925823845fb35cd7dc238a3e
Enzo <enzogamerxbox@outlook.com>
2019-01-31 21:02:13 -0300

    agregando espacios

--- ejemplo_while.py
+++ ejemplo_while.py
@@ -1,13 +1,24 @@
 #Ejemplo de contador while
 
-numero_inicial= int(input('Digite un numero'))
+numero_inicial= int(input('Digite un numero: '))
+print(" ")
+print(" ")
 print(numero_inicial)
+print(" ")
+print(" ")
 
 while numero_inicial > 0:
+    print(" ")
     numero_inicial -= 1
     print(numero_inicial)
+    print(" ")
+    print(" ")
 
     if numero_inicial % 2 == 0:
+        print(" ")
         print('Este numero es par')
+        print(" ")
     else:
+        print(" ")
         print('Este numero es impar')
+        print(" ")
